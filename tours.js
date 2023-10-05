4a0636c86ba7b717bb3913d02c28bde0d35f6a91
Giordano Temple <giordanotemple@gmail.com>
2023-10-05 13:33:02 -0400

    working on datastructures

---
+++ tours.js
@@ -0,0 +1,25 @@
+//testing how I will implement my data structure for tours
+
+const trip = {
+  dayOne: {
+    
+  },
+  dayTwo: {
+
+  },
+  dayThree: {
+    
+  }
+}
+
+/*
+  notes:
+    - use javasc
+*/
+
+/*
+Other columns to add:
+  number of buses - integer
+
+
+*/
