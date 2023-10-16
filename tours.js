a8d9dd3e2231f82f1d579b98ca3d969f297236be
Giordano Temple <giordanotemple@gmail.com>
2023-10-16 13:29:05 -0400

    update tour object

--- tours.js
+++ tours.js
@@ -1,15 +1,18 @@
 //testing how I will implement my data structure for tours
 
 const info = {
-  ad: [1, 2],
-  bus: {
-    amount: 2,
-    company: [1, 2]
+  "ad": {
+    "amount": 2,
+    "ids": [1, 2]
+  },
+  "bus": {
+    "amount": 2,
+    "company": [1, 2]
   }
 };
 
 const trip = {
-  days: {
+  "days: {
     1: {
       date: 'date',
       hotel: 1,
@@ -19,6 +22,12 @@ const trip = {
           end: time,
           description: description,
           id: 1
+        },
+        2: {
+          start: time,
+          end: time,
+          description: description,
+          id: 2
         }
       }
     }
