f18fbc32c909da4b54ed4693ec742023530234a6
Giordano Temple <giordanotemple@gmail.com>
2023-10-16 20:42:17 -0400

    add user tours

---
+++ app/pages/tours/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Tour = () => {
+  return (
+    <div>tour</div>
+  )
+}
+
+export default Tour;
--- app/pages/tours/user-tours/page.tsx
+++ app/pages/tours/user-tours/page.tsx
@@ -1,11 +1,52 @@
-import React from 'react'
-
 import { getToursByUserId } from '@/app/api/getTours';
+import Link from 'next/link'
+
+
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 const Tours = async () => {
-  const tours = await getToursByUserId('1');
+  const { tourData } = await getToursByUserId('1');
+
   return (
-    <div>page{console.log('tttooooouuuurs:', tours)}</div>
+    <div>
+      {tourData?.length > 0 && (
+        <div>
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {tourData.map((tour) => (
+                    <TableRow
+                      key={tour.id}
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell component="th" scope="row">{tour.schools.name}</TableCell>
+                      <TableCell>{tour.destination}</TableCell>
+                      <TableCell>{tour.start}</TableCell>
+                      <TableCell>{tour.end}</TableCell>
+                      <TableCell>
+                        <Link href={`/pages/tours/${tour.id}`}>
+                            View
+                        </Link>
+                      </TableCell>
+                    </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </div>
+      )}
+      <Link href="/">Back</Link>
+    </div>
   )
 }
 
