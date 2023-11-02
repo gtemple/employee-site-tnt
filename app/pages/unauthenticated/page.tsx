60528d52f356886654714de06eadcaa0c7818a34
Giordano Temple <giordanotemple@gmail.com>
2023-11-02 09:36:26 -0400

    fix

--- app/pages/unauthenticated/page.tsx
+++ app/pages/unauthenticated/page.tsx
@@ -1,17 +1,27 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
+import { cache } from 'react'
+
+export const createServerSupabaseClient = cache(() => {
+  const cookieStore = cookies()
+  return createServerComponentClient({ cookies: () => cookieStore })
+  })
 
 
 export default async function Unauthenticated() {
-  const supabase = createServerComponentClient({ cookies });
-  const { data: {session}} = await supabase.auth.getSession();
-
-  if (session) {
-    redirect('/pages/login')
+  const supabase = createServerSupabaseClient()
+  // const { data: {session}} = await supabase.auth.getSession();
+  try {
+    const {
+      data: { user },
+    } = await supabase.auth.getUser()
+    if (!user) {
+      redirect('/pages/login')
+    }
+    return user
+  } catch (error) {
+    console.log('Error --', error)
+    return null
   }
-  
-  return (
-    <div>Please sign in</div>
-    )
 }
