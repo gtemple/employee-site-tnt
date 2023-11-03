8c8a4d870376b0023cc19d26130fd4e1d8c5c79d
Giordano Temple <giordanotemple@gmail.com>
2023-11-03 11:43:55 -0400

    block signup

--- app/pages/signup/page.tsx
+++ app/pages/signup/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Login() {
   return (
     <div>
-      <Link href="/">Back</Link>
+      {/* <Link href="/">Back</Link>
 
       <form
         action="/api/auth/sign-up"
@@ -33,7 +33,8 @@ export default function Login() {
           required
         />
         <button>Sign up</button>
-      </form>
+      </form> */}
+      <div className='construction'>Thank you for your interest in registering. This site is currently not accepting new users. If you would like to reach the developer for a demo, please contact Giordano at giordanotemple@gmail.com</div>
       <Link href="/pages/login">Login</Link>
     </div>
   )
