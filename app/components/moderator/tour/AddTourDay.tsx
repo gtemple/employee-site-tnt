09ed62ffab0f4068b640edb316f4d9534c355da3
Giordano Temple <giordanotemple@gmail.com>
2023-11-08 09:03:14 -0500

    fix calendar styling

--- app/components/moderator/tour/AddTourDay.tsx
+++ app/components/moderator/tour/AddTourDay.tsx
@@ -1,16 +1,26 @@
 "use client";
 
 import { useState } from "react";
+import Calendar from "react-calendar";
+import '@/app/styles/Calendar.css'
+
+type ValuePiece = Date | null;
+
+type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 export const AddTourDay = () => {
+  const [date, setDate] = useState<Value>(new Date());
   const [addDayWidget, setAddDayWidget] = useState(false);
-  const handleOpenDayWidget = () => setAddDayWidget(true);
 
+  const data = {};
+
+  const handleOpenDayWidget = () => setAddDayWidget(true);
   const handleCloseDayWidget = (submit: boolean) => {
     if (!submit) {
       setAddDayWidget(false);
     }
     setAddDayWidget(false);
+    console.log(date)
     console.log("post request to add date");
   };
 
@@ -20,6 +30,7 @@ export const AddTourDay = () => {
         <button onClick={handleOpenDayWidget}>Add Day</button>
       ) : (
         <div>
+          <Calendar onChange={setDate} value={date} />
           <button onClick={() => handleCloseDayWidget(true)}>Submit</button>
           <button onClick={() => handleCloseDayWidget(false)}>Cancel</button>
         </div>
