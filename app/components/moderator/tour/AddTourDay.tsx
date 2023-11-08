7e225f8be33068ade30a07ae049da8067ffe78e9
Giordano Temple <giordanotemple@gmail.com>
2023-11-08 16:51:46 -0500

    complete add day

--- app/components/moderator/tour/AddTourDay.tsx
+++ app/components/moderator/tour/AddTourDay.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import Calendar from "react-calendar";
+import dayjs from "dayjs";
 import "@/app/styles/Calendar.css";
 
 type ValuePiece = Date | null;
@@ -10,22 +11,37 @@ type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 export const AddTourDay = () => {
   const [data, setData] = useState({});
-  const [date, setDate] = useState<Value>(new Date());
+  const [dateValue, setDateValue] = useState(dayjs());
   const [addDayWidget, setAddDayWidget] = useState(false);
 
-  const addDate = () => {
+  const startDateExists = () => {
+    const keys = Object.keys(data);
+    if (keys.length > 0) {
+      return true;
+    }
+    return false;
+  };
+
+  const startDate = () => {
     const keys = Object.keys(data);
     if (keys.length > 0) {
       return;
     }
-    setData(
-      {"1": {
-        date: date,
-      }}
-    );
-    console.log(keys);
-    console.log("post request to add date");
+    setData({
+      0: {
+        date: dateValue,
+      },
+    });
     return;
+  };
+
+  // this function adds a day to the schedule by doing the following:
+  // 1. gets current amount of days
+  // 2. adds the current trip length to the starting date (thus adding the next date... if start date is Nov 12th and trip length is already 3 days, 12 + 3 creates the 15th)
+  const addDay = () => {
+    const nextDayOnTrip = Object.keys(data).length;
+    const nextDate = dayjs(dateValue).add(nextDayOnTrip, "day").toDate();
+    setData((prev) => ({ ...prev, [nextDayOnTrip]: { date: nextDate } }));
   };
 
   const handleOpenDayWidget = () => setAddDayWidget(true);
@@ -33,18 +49,21 @@ export const AddTourDay = () => {
     if (!submit) {
       setAddDayWidget(false);
     }
-    addDate();
+    startDate();
     setAddDayWidget(false);
   };
 
   return (
     <div>
       <button onClick={() => console.log(data)}>test data</button>
-      {!addDayWidget ? (
-        <button onClick={handleOpenDayWidget}>Add Day</button>
+      {/* checks to see if there is already a start date, if so gives the option to add the following day */}
+      {startDateExists() ? (
+        <button onClick={addDay}>Add Day</button>
+      ) : !addDayWidget ? (
+        <button onClick={handleOpenDayWidget}>Set Start Date</button>
       ) : (
         <div>
-          <Calendar onChange={setDate} value={date} />
+          <Calendar onChange={setDateValue} value={dateValue} />
           <button onClick={() => handleCloseDayWidget(true)}>Submit</button>
           <button onClick={() => handleCloseDayWidget(false)}>Cancel</button>
         </div>
