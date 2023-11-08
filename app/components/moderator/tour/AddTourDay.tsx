a881f3c17923ddc02eb3aee66e526133d9065148
Giordano Temple <giordanotemple@gmail.com>
2023-11-08 09:27:25 -0500

    update days to tour progress

--- app/components/moderator/tour/AddTourDay.tsx
+++ app/components/moderator/tour/AddTourDay.tsx
@@ -2,30 +2,44 @@
 
 import { useState } from "react";
 import Calendar from "react-calendar";
-import '@/app/styles/Calendar.css'
+import "@/app/styles/Calendar.css";
 
 type ValuePiece = Date | null;
 
 type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 export const AddTourDay = () => {
+  const [data, setData] = useState({});
   const [date, setDate] = useState<Value>(new Date());
   const [addDayWidget, setAddDayWidget] = useState(false);
 
-  const data = {};
+  const addDate = () => {
+    const keys = Object.keys(data);
+    if (keys.length > 0) {
+      return;
+    }
+    setData(
+      {"1": {
+        date: date,
+      }}
+    );
+    console.log(keys);
+    console.log("post request to add date");
+    return;
+  };
 
   const handleOpenDayWidget = () => setAddDayWidget(true);
   const handleCloseDayWidget = (submit: boolean) => {
     if (!submit) {
       setAddDayWidget(false);
     }
+    addDate();
     setAddDayWidget(false);
-    console.log(date)
-    console.log("post request to add date");
   };
 
   return (
     <div>
+      <button onClick={() => console.log(data)}>test data</button>
       {!addDayWidget ? (
         <button onClick={handleOpenDayWidget}>Add Day</button>
       ) : (
