499ba14dea86231fb01bc508153ff614edd6dec8
Giordano Temple <giordanotemple@gmail.com>
2023-11-22 13:20:08 -0500

    remove console log

--- app/pages/moderator/tours/edit/[id]/page.tsx
+++ app/pages/moderator/tours/edit/[id]/page.tsx
@@ -9,7 +9,6 @@ import { getTourData } from "@/app/api/tours/getTours";
 import { AddTourDay } from "@/app/components/moderator/tour/AddTourDay";
 
 export const EditTour = async ({ params }: Params) => {
-  console.log('hi')
   try {
     // Make all async calls concurrently using Promise.all
     const [schools, destinations, hotels, restaurants, sites] =
@@ -49,36 +48,3 @@ export const EditTour = async ({ params }: Params) => {
 };
 
 export default EditTour;
-
-/*
-
-{
-  id: 2,
-  created_at: '2023-11-21T15:20:57.152237+00:00',
-  itinerary: {
-    '1': { date: '2023-11-21T05:00:00.000Z', schedule: [Object] },
-    '2': { date: '2023-11-22T05:00:00.000Z', schedule: [Object] }
-  },
-  start: '2023-11-21',
-  end: '2023-11-23',
-  school_id: 1,
-  notes: null,
-  info: null,
-  students: 20,
-  destination_id: 1,
-  schools: {
-    id: 1,
-    created_at: '2023-10-04T18:25:32.332452+00:00',
-    name: 'Northlea Public School',
-    grade: '8',
-    address: '305 Rumsey Rd',
-    city: 'Toronto',
-    postal: 'ON M4G 1R4',
-    teacher: 'G. Temple',
-    notes: null,
-    phone: '(416) 555-5555',
-    board: 1
-  }
-}
-
-*/
