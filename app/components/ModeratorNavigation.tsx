10f1c2846f4605aba56a152613b0bbb47fde594f
Giordano Temple <giordanotemple@gmail.com>
2023-12-12 14:34:37 -0500

    improve moderator nav style

--- app/components/ModeratorNavigation.tsx
+++ app/components/ModeratorNavigation.tsx
@@ -6,15 +6,18 @@ const ModeratorNavigation = async () => {
   const user = await getUser();
 
   //@ts-ignore
-  const profile = user[0]
+  const profile = user[0];
   const mod = profile.moderator === true ? true : false;
   return mod ? (
     <div className="moderator-dash">
-      <Link href="/pages/sites">Sites</Link>
-      <Link href="/pages/moderator/restaurants">Restaurants</Link>
-      <Link href="/pages/moderator/tours">Tours</Link>
-      <Link href="/pages/moderator/schools">Schools</Link>
-      <Link href="/pages/moderator/hotels">Hotels</Link>
+      <div>MODERATOR TOOLS</div>
+      <div className="moderator-menu">
+        <Link href="/pages/sites">Sites</Link>
+        <Link href="/pages/moderator/restaurants">Restaurants</Link>
+        <Link href="/pages/moderator/tours">Tours</Link>
+        <Link href="/pages/moderator/schools">Schools</Link>
+        <Link href="/pages/moderator/hotels">Hotels</Link>
+      </div>
     </div>
   ) : null;
 };
