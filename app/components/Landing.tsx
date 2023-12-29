2064de9a15b820bc8358c97deba8d3dd52b5ee5c
Giordano Temple <giordanotemple@gmail.com>
2023-12-29 16:15:55 -0500

    landing styling

--- app/components/Landing.tsx
+++ app/components/Landing.tsx
@@ -6,6 +6,13 @@ const Landing = () => {
   return (
     <div className='landing'>
       <div>
+      {/* <Image
+          src="/images/landing-background.jpeg"
+          width={600}
+          height={450}
+          alt="Temple and Temple staff"
+          className='landing-image'
+        /> */}
         <Image
           src="/images/temple-logo2.png"
           width={600}
@@ -13,6 +20,7 @@ const Landing = () => {
           alt="Temple and Temple Tours"
         />
         </div>
+        <div className='landing-text'>
       <div className='landing-description'>Activities Director Site</div>
       <div className='landing-moto'>Your travels made easy.</div>
       <div className="landing-btns-container">
@@ -22,6 +30,7 @@ const Landing = () => {
       <div className='landing-footer'>
         The Temple and Temple Tours Activities Director aims to help all Temple and Temple staff feel more confident in their tasks.
         Not only will it aid operations in organizing trips, but both new and experienced ADs will be prepared for upcoming tours.
+      </div>
       </div>
     </div>
   );
